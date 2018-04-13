525eabdbc514c67f14c07fca15251d0c5e180386
Lars <lars.scheme@gmail.com>
2018-04-13 19:53:57 +0200

    Move worm around with arrow keys

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image"
 	"runtime"
+	"sort"
 	"time"
 	"unsafe"
 
@@ -26,22 +27,6 @@ func main() {
 	window, err := win.NewWindow(opts, msg.Callback)
 	check(err)
 	var windowedPlacement w32.WINDOWPLACEMENT
-	msg.OnKeyDown = func(key uintptr, opt win.KeyOptions) {
-		switch key {
-		case w32.VK_SPACE:
-			if !opt.WasDown() {
-				updatePartialTexture()
-			}
-		case w32.VK_F11:
-			if win.IsFullscreen(window) {
-				win.DisableFullscreen(window, windowedPlacement)
-			} else {
-				windowedPlacement = win.EnableFullscreen(window)
-			}
-		case w32.VK_ESCAPE:
-			win.CloseWindow(window)
-		}
-	}
 
 	// set up D3D9
 	d3d, err := d3d9.Create(d3d9.SDK_VERSION)
@@ -145,14 +130,16 @@ func main() {
 
 	renderTex := func(device *d3d9.Device, tex *d3d9.Texture, x, y, width, height int) {
 		device.SetTexture(0, tex)
+		xf := float32(x)
+		yf := float32(y)
 		w := float32(width)
 		h := float32(height)
 		v := []float32{
 			// x, y, z, w, u, v
-			0 - 0.5, h - 0.5, 0, 1, 0, 1,
-			w - 0.5, h - 0.5, 0, 1, 1, 1,
-			0 - 0.5, 0 - 0.5, 0, 1, 0, 0,
-			w - 0.5, 0 - 0.5, 0, 1, 1, 0,
+			xf + 0 - 0.5, yf + h - 0.5, 0, 1, 0, 1,
+			xf + w - 0.5, yf + h - 0.5, 0, 1, 1, 1,
+			xf + 0 - 0.5, yf + 0 - 0.5, 0, 1, 0, 0,
+			xf + w - 0.5, yf + 0 - 0.5, 0, 1, 1, 0,
 		}
 		device.DrawPrimitiveUP(
 			d3d9.PT_TRIANGLESTRIP,
@@ -160,6 +147,65 @@ func main() {
 			uintptr(unsafe.Pointer(&v[0])),
 			floatsPerVertex*4,
 		)
+	}
+
+	// create worm
+	worm := parseWorm("worm.xcf")
+	swapRB(worm.left)
+	swapRB(worm.right)
+	swapRB(worm.hitboxImg)
+	worm.leftTex, err = rgbaToTexture(device, worm.left)
+	check(err)
+	defer worm.leftTex.Release()
+	worm.rightTex, err = rgbaToTexture(device, worm.right)
+	check(err)
+	defer worm.rightTex.Release()
+	{
+		p := worm.hitboxImg.Pix
+		for i := 3; i < len(p); i += 4 {
+			p[i] /= 4
+		}
+	}
+	worm.hitboxTex, err = rgbaToTexture(device, worm.hitboxImg)
+	check(err)
+	defer worm.hitboxTex.Release()
+	worm.x, worm.y = 110, 15
+
+	moveWorm := func(dx, dy int) {
+		x, y := worm.x+dx, worm.y+dy
+		for _, p := range worm.hitbox {
+			if level.RGBAAt(x+p.x, y+p.y).A > 127 {
+				return
+			}
+		}
+		worm.x, worm.y = x, y
+	}
+
+	msg.OnKeyDown = func(key uintptr, opt win.KeyOptions) {
+		if opt.WasDown() {
+			return
+		}
+
+		switch key {
+		case w32.VK_RIGHT:
+			moveWorm(1, 0)
+		case w32.VK_LEFT:
+			moveWorm(-1, 0)
+		case w32.VK_UP:
+			moveWorm(0, -1)
+		case w32.VK_DOWN:
+			moveWorm(0, 1)
+		case w32.VK_SPACE:
+			updatePartialTexture()
+		case w32.VK_F11:
+			if win.IsFullscreen(window) {
+				win.DisableFullscreen(window, windowedPlacement)
+			} else {
+				windowedPlacement = win.EnableFullscreen(window)
+			}
+		case w32.VK_ESCAPE:
+			win.CloseWindow(window)
+		}
 	}
 
 	// run main game loop
@@ -188,6 +234,16 @@ func main() {
 			0, 0,
 			level.Bounds().Dx(), level.Bounds().Dy(),
 		)
+		renderTex(
+			device, worm.leftTex,
+			worm.x, worm.y,
+			worm.left.Bounds().Dx(), worm.left.Bounds().Dy(),
+		)
+		renderTex(
+			device, worm.hitboxTex,
+			worm.x, worm.y,
+			worm.left.Bounds().Dx(), worm.left.Bounds().Dy(),
+		)
 
 		check(device.SetSamplerState(0, d3d9.SAMP_MINFILTER, d3d9.TEXF_LINEAR))
 		check(device.SetSamplerState(0, d3d9.SAMP_MAGFILTER, d3d9.TEXF_LINEAR))
@@ -195,7 +251,7 @@ func main() {
 		check(err)
 		defer bb.Release()
 		check(device.SetRenderTarget(0, bb))
-		scale *= 1.005
+		scale = 3.5
 		renderTex(device, backbuf, 0, 0, int(scale*400+0.5), int(scale*200+0.5))
 
 		device.EndScene()
@@ -261,3 +317,87 @@ func updatePartialRect(tex *d3d9.Texture, img *image.RGBA, left, top, width, hei
 	)
 	return tex.UnlockRect(0)
 }
+
+type worm struct {
+	hitbox            []point
+	hitboxImg         *image.RGBA
+	hitboxTex         *d3d9.Texture
+	left, right       *image.RGBA
+	leftTex, rightTex *d3d9.Texture
+	x, y              int
+}
+
+func parseWorm(xcfFile string) worm {
+	canvas, err := xcf.LoadFromFile(xcfFile)
+	check(err)
+	return worm{
+		hitbox:    parseHitbox(canvas.GetLayerByName("hitbox")),
+		hitboxImg: canvas.GetLayerByName("hitbox").RGBA,
+		left:      canvas.GetLayerByName("worm left").RGBA,
+		right:     canvas.GetLayerByName("worm right").RGBA,
+	}
+}
+
+func parseHitbox(img *xcf.Layer) []point {
+	outline := make(map[point]bool)
+
+	b := img.Bounds()
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			if img.RGBAAt(x, y).A > 0 {
+				outline[pt(x, y)] = true
+				break
+			}
+		}
+
+		for x := b.Max.X - 1; x >= b.Min.X; x-- {
+			if img.RGBAAt(x, y).A > 0 {
+				outline[pt(x, y)] = true
+				break
+			}
+		}
+	}
+
+	for x := b.Min.X; x < b.Max.X; x++ {
+		for y := b.Min.Y; y < b.Max.Y; y++ {
+			if img.RGBAAt(x, y).A > 0 {
+				outline[pt(x, y)] = true
+				break
+			}
+		}
+
+		for y := b.Max.Y - 1; y >= b.Min.Y; y-- {
+			if img.RGBAAt(x, y).A > 0 {
+				outline[pt(x, y)] = true
+				break
+			}
+		}
+	}
+
+	points := make([]point, 0, len(outline))
+	for p, _ := range outline {
+		points = append(points, p)
+	}
+	sort.Sort(byX(points))
+	sort.Stable(byY(points))
+
+	return points
+}
+
+type point struct {
+	x, y int
+}
+
+func pt(x, y int) point { return point{x: x, y: y} }
+
+type byX []point
+
+func (p byX) Len() int           { return len(p) }
+func (p byX) Less(i, j int) bool { return p[i].x < p[j].x }
+func (p byX) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+type byY []point
+
+func (p byY) Len() int           { return len(p) }
+func (p byY) Less(i, j int) bool { return p[i].y < p[j].y }
+func (p byY) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
